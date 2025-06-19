edf440ec6d658486440d8f6f01241b798c5fd64b
robiulsagor <robiul100.me@gmail.com>
2025-06-19 21:09:15 +0600

    Hero section with navbar complete

---
+++ app/components/Hero.tsx
@@ -0,0 +1,56 @@
+import Image from "next/image"
+import image from '../../public/pic.jpg'
+import logo1 from '../../public/Logo 1.png'
+import logo2 from '../../public/Logo 2.png'
+import logo3 from '../../public/Logo 3.png'
+import logo4 from '../../public/Logo 4.png'
+import logo5 from '../../public/Logo 5.png'
+import Navbar from "./Navbar"
+import { IoChevronForwardOutline } from "react-icons/io5"
+
+const Hero = () => {
+    return (
+        <div className="bg-[#080808]">
+            <Navbar />
+
+            <div className="max-w-6xl mx-auto px-4 py-8">
+
+                <div className="md:flex items-center justify-between ">
+                    <div className="flex-1">
+                        <Image
+                            src={image}
+                            alt='Robiul Islam'
+                            height={400}
+                            width={400}
+                            className="rounded-full w-[300px] h-[300px] md:w-[400px] md:h-[400px] object-cover mx-auto md:mx-0"
+                        />
+                    </div>
+                    <div className="flex-1 space-y-7 mt-6">
+                        <h2 className="text-3xl md:text-4xl lg:text-5xl text-gray-200 font-bold">Robiul Islam Sagar</h2>
+                        <p className="text-gray-400 leading-7 tracking-wider">I’m a self-taught MERN stack developer with a strong focus on building real-world, fullstack web applications. With a passion for clean UI and efficient backend logic, I love turning ideas into responsive, user-friendly products. </p>
+
+                        <button className="bg-[#3F8E00] rounded py-3 px-8 text-white text-xl flex gap-2 items-center cursor-pointer group mx-auto md:mx-0">
+                            Let&apos;s Get Started <IoChevronForwardOutline className="group-hover:translate-x-3 transition" />
+                        </button>
+                    </div>
+
+
+                </div>
+
+                <div className="pb-10 pt-10">
+                    <h3 className="text-gray-200 text-xl">Worked With</h3>
+
+                    <div className="flex justify-around md:justify-between items-center flex-wrap mt-4 gap-4">
+                        <Image src={logo1} alt="logo1" width={190} height={200} className="w-[150px]" />
+                        <Image src={logo2} alt="logo1" width={190} height={200} className="w-[150px]" />
+                        <Image src={logo3} alt="logo1" width={190} height={200} className="w-[150px]" />
+                        <Image src={logo4} alt="logo1" width={190} height={200} className="w-[150px]" />
+                        <Image src={logo5} alt="logo1" width={190} height={200} className="w-[150px]" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { BiMenu, BiX } from "react-icons/bi"
+import { BsBehance, BsTwitter } from "react-icons/bs"
+import { LuLinkedin } from "react-icons/lu"
+
+const data = [
+    {
+        id: 1,
+        name: "Home",
+        url: "/",
+    },
+    {
+        id: 2,
+        name: "Case Studies",
+        url: "/#case-studies",
+    },
+    {
+        id: 3,
+        name: "Testimonials",
+        url: "/#testimonials",
+    },
+    {
+        id: 4,
+        name: "Recent Works",
+        url: "/#recent-works",
+    },
+    {
+        id: 5,
+        name: "Get in touch",
+        url: "/#contact",
+    },
+]
+
+const Navbar = () => {
+    const [showMenu, setShowMenu] = useState(false);
+    const handleMenuToggle = () => {
+        setShowMenu(!showMenu);
+    };
+
+    useEffect(() => {
+        if (showMenu) {
+            document.body.style.overflowY = 'hidden'
+        } else {
+            document.body.style.overflowY = 'auto'
+        }
+    }, [showMenu])
+
+    return (
+        <div className="max-w-7xl bg-[#1b1b1b] mx-auto rounded-bl-lg rounded-br-lg py-3 md:py-6 px-5">
+            <div className="max-w-6xl mx-auto flex items-center justify-between gap-14">
+                {/* desktop nav */}
+                <div className="flex-1 hidden md:flex items-center justify-between text-slate-400">
+                    {
+                        data.map(item => (
+                            <Link key={item.id} href={item.url} className="hover:text-slate-200 transition">{item.name}</Link>
+                        ))
+                    }
+                </div>
+
+                {/* mobile nav button */}
+                <div className="md:hidden ms-auto z-50">
+                    <button className="ml-auto">
+                        {showMenu ? <BiX className="text-3xl text-slate-500 cursor-pointer" onClick={handleMenuToggle} /> : <BiMenu className="text-3xl text-slate-500 cursor-pointer" onClick={handleMenuToggle} />}
+                    </button>
+                </div>
+
+                {/* mobile nav */}
+                <div className={`absolute top-15 left-0 w-full bg-[#1b1b1b] rounded-bl-lg rounded-br-lg z-30 ${showMenu ? '' : '-translate-y-64 opacity-0'} transition duration-300`}>
+                    <div className="flex flex-col items-center justify-center gap-8 py-8 border-t-slate-800 border-t">
+                        {
+                            data.map(item => (
+                                <Link key={item.id} href={item.url} className="text-slate-400 hover:text-slate-200 transition">{item.name}</Link>
+                            ))
+                        }
+                    </div>
+                </div>
+
+
+                <div className="hidden lg:flex gap-10 items-center ">
+                    <LuLinkedin className="text-slate-400 hover:text-slate-200 cursor-pointer transition" size={22} />
+                    <BsBehance className="text-slate-400 hover:text-slate-200 cursor-pointer transition" size={22} />
+                    <BsTwitter className="text-slate-400 hover:text-slate-200 cursor-pointer transition" size={22} />
+                </div>
+            </div>
+
+            {/* menu overlay for mobile nav */}
+            {
+                showMenu && <div className="absolute top-16 left-0 w-full h-screen backdrop-blur-sm bg-black/50"
+                    onClick={() => setShowMenu(false)}></div>
+            }
+        </div>
+    )
+}
+
+export default Navbar
